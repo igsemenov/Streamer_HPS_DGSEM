d86380a0f8bcfab4d7a99ddeff49f98a79cbadcd
Igor L. Semenov <89221389+igsemenov@users.noreply.github.com>
2021-09-06 16:29:23 +0200

    Update pycode_int_1.py

--- test_martinnson/pycode_int_1.py
+++ test_martinnson/pycode_int_1.py
@@ -1,1 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import pylib_lgn as lg
+###############################################################################
+def test_solve(n):
 
+    lgn=lg.lgn1d(n)
+
+    nod=lg.nod2d([0.,0.],[1.,1.],lgn)
+
+    grn=lg.elm2d(lgn)
+
+###############################################################################
+
+    x=nod.x[:,0]
+    y=nod.y[:,0]
+    
+    Re=-2.*4.*np.sin(2.*x+2.*y)
+
+###############################################################################
+
+    Ae=grn.ope.Lx+grn.ope.Ly
+
+    Ae=np.linalg.solve(Ae,np.eye(n*n))
+
+    Ab=-grn.opb.Lx-grn.opb.Ly
+
+    G=Ae.dot(Ab)
+    g=Ae.dot(Re)
+
+###############################################################################
+
+    xe,ye=nod.getedg()
+    
+    xe=np.hstack(xe)
+    ye=np.hstack(ye)
+    
+    F=np.sin(2.*xe+2.*ye)
+    
+    f=grn.ope.Q.dot(g)+(grn.ope.Q.dot(G)+grn.opb.Q).dot(F)
+    
+    gx=grn.ope.Ux.dot(g)+(grn.ope.Ux.dot(G)+grn.opb.Ux).dot(F)
+    gy=grn.ope.Uy.dot(g)+(grn.ope.Uy.dot(G)+grn.opb.Uy).dot(F)
+
+###############################################################################
+
+    err=np.amax(abs(f-np.sin(2.*x+2.*y)))
+    
+    err_x=np.amax(abs(gx-2.*np.cos(2.*x+2.*y)))
+    err_y=np.amax(abs(gy-2.*np.cos(2.*x+2.*y)))
+
+    return np.array([n,err,err_x,err_y])
+
+###############################################################################
+
+err=list()
+
+for n in [4,6,8,10,12,14,16,18]:
+    err.append(test_solve(n))
+
+err=np.stack(err)
+
+np.savetxt("err_int_1.txt",err)
+
+###############################################################################
