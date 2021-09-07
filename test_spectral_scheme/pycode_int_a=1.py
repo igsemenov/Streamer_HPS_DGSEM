2c87791e1f536e4dfa122b21be596b6eb18b40b1
Igor L. Semenov <89221389+igsemenov@users.noreply.github.com>
2021-09-07 10:36:22 +0200

    Update pycode_int_a=1.py

--- test_spectral_scheme/pycode_int_a=1.py
+++ test_spectral_scheme/pycode_int_a=1.py
@@ -1,9 +1,26 @@
 ###############################################################################
-" pycode_int_a=1 "
-###############################################################################
 import numpy as np
 import pylib_lgn as lg
 from scipy.special import jv
+###############################################################################
+"""
+Function: test_solve(n)
+
+    Implements the IMR scheme for solving Eq. (B.1) at alpha=1
+
+Inputs:
+    
+    n: the number of Gauss nodes per direction
+
+Returns:
+
+    err: error in the solution
+    err_x: error in the derivative with respect to x
+    err_y: error in the derivative with respect to y
+
+Notes: the error is defined using the L-infinity norm
+
+"""
 ###############################################################################
 def test_solve(n):
 
@@ -62,6 +79,6 @@ def test_solve(n):
     err_x=np.amax(abs(gx-gx_ref))
     err_y=np.amax(abs(gy-gy_ref))
 
-    return np.array([n,err,err_x,err_y])
+    return err,err_x,err_y
 
 ###############################################################################
