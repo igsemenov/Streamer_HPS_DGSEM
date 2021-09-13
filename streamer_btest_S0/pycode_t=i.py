df0992e468e00f69ec48499dc2b2d7e566a23475
Igor L. Semenov <89221389+igsemenov@users.noreply.github.com>
2021-09-13 22:28:51 +0200

    Add files via upload

---
+++ streamer_btest_S0/pycode_t=i.py
@@ -0,0 +1,70 @@
+###############################################################################
+" pycode: initial, t=0 "
+###############################################################################
+import pylib_hps as hp
+from pylib_hps import np
+###############################################################################
+"""
+
+This script initializes the mesh and solution at t=0
+
+The data are saved to pycode/time_0
+
+"""
+###############################################################################
+def mask_add(root):
+
+    if root.p[0]==0:
+        return True
+    else:
+        return False
+
+###############################################################################
+
+lgn=hp.lg.lgn2d(6,4)
+fvs=hp.lg.fvs2d(lgn)
+dgs=hp.lg.dgs2d(lgn)
+grn=hp.lg.elm2d(lgn)
+
+hps=hp.unit([50.,50.],[50.,50.])
+
+hps.add(mask_add)
+hps.add(mask_add)
+hps.add(mask_add)
+hps.add(mask_add)
+hps.add(mask_add)
+hps.add(mask_add)
+
+hps.getelm()
+
+###############################################################################
+
+hps.setnod_lgn(lgn)
+
+hps.setfld_fvs(fvs)
+hps.setfld_dgs(dgs)
+
+for elm in hps.elm:
+
+    fld=elm.fld_fvs
+
+    fld.f[0][:,:]=1e-6
+    fld.f[2][:,:]=1e-6
+
+    fld.f[0]=fld.f[0]+np.exp(-(fld.x/2.1)**2-((fld.y-50.0)/2.7)**2)
+    fld.f[2]=fld.f[2]+np.exp(-(fld.x/2.1)**2-((fld.y-50.0)/2.7)**2)
+
+    nod=elm.nod_lgn
+    fld=elm.fld_dgs
+
+    fld.f[0][:,:]=1e-6
+    fld.f[2][:,:]=1e-6
+
+    fld.f[0]=fld.f[0]+np.exp(-(nod.x/2.1)**2-((nod.y-50.0)/2.7)**2)
+    fld.f[2]=fld.f[2]+np.exp(-(nod.x/2.1)**2-((nod.y-50.0)/2.7)**2)
+
+###############################################################################
+
+hps.save("pydata/time_0")
+
+###############################################################################
---
+++ streamer_btest_S0/pycode_t=s.py
@@ -0,0 +1,62 @@
+###############################################################################
+" pycode: step, t>0 "
+###############################################################################
+import pylib_hps as hp
+#------------------------------------------------------------------------------
+import time
+###############################################################################
+"""
+
+This script simulates the streamer propagation on the time interval 0.5 ns
+
+The initial state is loaded from pydata\time_#, where #=time_moment
+
+The computed results are saved to pydata\time_#, where #=time_moment+1
+
+"""
+###############################################################################
+time_moment=4
+#------------------------------------------------------------------------------
+dt=2e-3
+mt=5
+nt=50
+###############################################################################
+
+lgn=hp.lg.lgn2d(6,4)
+fvs=hp.lg.fvs2d(lgn)
+dgs=hp.lg.dgs2d(lgn)
+grn=hp.lg.elm2d(lgn)
+
+path="pydata/time_"+str(time_moment)
+
+hps=hp.pickle.load(open(path+"/hps.bin"))
+
+hps.load(path,dgs,fvs,lgn)
+
+hps.getelm()
+
+hps.rib_dgs_fvs()
+
+hps.setpsn(grn)
+hps.psndtn(grn)
+
+###############################################################################
+
+tic=time.time()
+
+#for s in range(nt):
+#    print("s="+str(s+1))
+#    hps.step_dgs_fvs(dt,mt,dgs,fvs,lgn)
+
+toc=time.time()
+print("run time: "+str(toc-tic))
+
+###############################################################################
+
+hps.step_psn(lgn)
+
+sol=hps.getsol(4,lgn,fvs)
+
+#hps.save("pydata\\time_"+str(time_moment+1))
+
+###############################################################################
