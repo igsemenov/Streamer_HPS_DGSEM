6354c9bbb2b729be846e25755fa2684d2a24dd9e
Igor L. Semenov <89221389+igsemenov@users.noreply.github.com>
2021-09-13 14:59:43 +0200

    Update pycode_test_2.2.py

--- test_fvs/pycode_test_2.2.py
+++ test_fvs/pycode_test_2.2.py
@@ -14,7 +14,7 @@ Introduction:
 
     The considered test problem is Test 2.2.
 
-    The computation is performed for until  t=1.
+    The computation is run till  t=1.
 
     Use plotelm() to plot the grid nodes
 
